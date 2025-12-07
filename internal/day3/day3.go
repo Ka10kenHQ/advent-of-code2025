9453eedb8e169091f859f69577df8beb63ed2adf
mate <matekopaliani12@gmail.com>
2025-12-07 21:41:35 +0400

    day3 done

--- internal/day3/day3.go
+++ internal/day3/day3.go
@@ -1,69 +1,76 @@
 package day3
 
 import (
-	"math"
+	"fmt"
 	"strconv"
 )
 
-func Run(lines []string) (int, int) {
+func Run(lines []string) (int64, int64) {
 	ans1 := partOne(lines)
 	ans2 := partTwo(lines)
 
 	return ans1, ans2
 }
 
-func partOne(lines []string) int {
-	joltageSum := 0
+func partOne(lines []string) int64 {
+	var joltageSum int64 = 0
 	for _, line := range lines {
-		joltageSum += processLine(line)
+		joltageSum += processLine(line, 2)
 	}
 	return joltageSum
 }
 
-func partTwo(lines []string) int {
-	return 0
+func partTwo(lines []string) int64 {
+	var joltageSum int64 = 0
+	for _, line := range lines {
+		joltageSum += processLine(line, 12)
+	}
+	return joltageSum
 }
 
-func processLine(line string) int {
-	size := len(line)
-	arr := make([]int, size)
+func processLine(line string, n int64) int64 {
+	var size int64 = int64(len(line))
+	arr := make([]int64, size)
 
 	for i := range size {
 		raw_int, err := strconv.Atoi(string(line[i]))
-
-		if err != nil { panic("Error: Converting")
+		if err != nil { panic("Error: Converting") }
+		arr[i] = int64(raw_int)
+	}
+	
+	result := make([]int64, 0, n)
+	var numToSkip int64 = size - n
+	
+	for i := range size {
+		for len(result) > 0 && result[len(result)-1] < arr[i] && numToSkip > 0 {
+			result = result[:len(result)-1]
+			numToSkip--
 		}
-
-		arr[i] = raw_int
+		result = append(result, arr[i])
 	}
+	
+	result = result[:n]
+	
+	fmt.Println(result)
+	ans := buildAnswer(result, n)
+	fmt.Println(ans)
+	return ans
+}
 
-	curr_max := arr[0]
-	second_best := math.MinInt32
 
-	curr_max_i := 0
-
-	for i := 1; i < size; i++ {
-		if arr[i] > curr_max {
-			curr_max = arr[i]
-			curr_max_i = i
-		}
+func buildAnswer(digits []int64, n int64) int64 {
+	var ans int64 = 0
+	for i := range n {
+		power := n - 1 - i
+		ans += digits[i] * pow10(power)
 	}
+	return ans
+}
 
-	starting_point := curr_max_i + 1
-
-	if curr_max_i == size - 1 {
-		starting_point = 0
+func pow10(n int64) int64 {
+	var result int64 = 1
+	for range n {
+		result *= 10
 	}
-
-	for i := starting_point; i < size; i ++ {
-		if second_best < arr[i] && second_best <= curr_max && curr_max_i != i {
-			second_best = arr[i]
-		}
-	}
-
-	if curr_max_i == size - 1 {
-		return second_best * 10 + curr_max
-	}
-
-	return curr_max * 10 + second_best
+	return result
 }
